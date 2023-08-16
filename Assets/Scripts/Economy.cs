b624b8d234ff7e35531640034b87cffccf14c26d
Nick Richards <nrichards@gmail.com>
2023-08-16 14:06:28 -0700

    Add snake over time

--- Assets/Scripts/Economy.cs
+++ Assets/Scripts/Economy.cs
@@ -8,17 +8,37 @@ public class Economy : MonoBehaviour
     public TMPro.TMP_Text SnakeCounter;
     public Inventory inventory;
     public CannonControl snakeCannon;
+    public float AddSnakeTime = 5f;
+    
+    public float snakeGrowTimer;
+    private IEnumerator countup;
+    private int waitCounter;
     
     // Garden // grow / sell / notify on damage
     
     void Start()
     {
         // snakeCannon.Launched += OnCannonLaunchSnake;
+        StartCoroutine(countup = Countup());
+        snakeGrowTimer = waitCounter;
     }
 
+    private IEnumerator Countup() {
+        while(true) {
+            yield return new WaitForSeconds(1);
+            waitCounter += 1;
+        }
+    }
+    
     void Update()
     {
         UpdateSnakeCounter();
+        
+        if ((waitCounter - snakeGrowTimer) > AddSnakeTime)
+        {
+            AddMoreSnake();
+            snakeGrowTimer = waitCounter;
+        }
     }
     
     void UpdateSnakeCounter()
@@ -29,5 +49,10 @@ public class Economy : MonoBehaviour
     void OnCannonLaunchSnake()
     {
         
+    }
+    
+    void AddMoreSnake()
+    {
+        inventory.AddSnake();
     }
 }
